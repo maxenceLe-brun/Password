d8d946e10a9b4087f9f81a43017bb2055760bb48
Maxence Le Brun <115214683+maxenceLe-brun@users.noreply.github.com>
2023-01-23 17:00:43 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+from time import*
+import hashlib
+print('"SEE" for check your passwords and pwd for entering a new password :')
+test = input()
+if test.upper() == "SEE":
+    file = open("password.json","r")
+    print(file.read())
+    file.close()
+elif test.lower() == "pwd":
+    pwd = input("set a password : ")
+    if len(pwd)>7 and pwd.upper() != pwd and pwd.lower() != pwd and True in [a in [str(b) for b in range(10)] for a in pwd] and True in [["!", "@", "#", "$", "%", "^", "&", "*"][a] in pwd for a in range(8)]:
+        print("YES")
+    else:
+        while True:
+            print("you need 8 characters, with an upper and a lower letter, a number and a special charater like : ")
+            print(" ! , @ , # , $ , % , ^ , & , * ")
+            sleep(1)
+            pwd = input("set a password : ")
+            if len(pwd)>7 and pwd.upper() != pwd and pwd.lower() != pwd and True in [a in [str(b) for b in range(10)] for a in pwd] and True in [["!", "@", "#", "$", "%", "^", "&", "*"][a] in pwd for a in range(8)]:
+                break
+    result = hashlib.sha256()
+    result.update(eval('b"'+pwd+'"'))
+    file = open("password.json","a")
+    file.close()
+    file = open("password.json","r")
+    track = file.read()
+    file.close()
+    if "password" not in eval(track):
+        reset = {"password": []}
+    else:
+        reset = eval(track)
+    if result.hexdigest() not in reset["password"]:
+        reset["password"] += [result.hexdigest()]
+        reset = str(reset)
+        file = open("password.json","w")
+        file.write(reset)
+        file.close()
+    else:
+        print("you already added this password")
+        
